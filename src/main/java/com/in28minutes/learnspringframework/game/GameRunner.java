aad448f22bdcae718fe4c6f2ea4575a58771977e
22sonamu <22sonamu@naver.com>
2023-05-26 10:40:42 +0900

    feat : SuperContraGame 추가 , 강한 결합의 문제점

--- src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
+++ src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
@@ -1,9 +1,12 @@
 package com.in28minutes.learnspringframework.game;
 
 public class GameRunner {
-    MarioGame game;
+    // MarioGame game;
+    // 강한 결합 , MarioGame이 아니면 Game을 Run 할수 없다.
+    // SuperContraGame 을 실행하려면 코드를 변경해야한다.
 
-    public GameRunner(MarioGame game){
+    private SuperContraGame game;
+    public GameRunner(SuperContraGame game){
         this.game = game;
     }
 
