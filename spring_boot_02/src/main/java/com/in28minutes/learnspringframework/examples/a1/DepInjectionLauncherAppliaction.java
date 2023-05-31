16738bda332a7256c3cb5016c8b6dc232f0a80be
22sonamu <22sonamu@naver.com>
2023-05-31 15:48:11 +0900

    의존성 주입의 다양한 유형 2 #1

--- spring_boot_02/src/main/java/com/in28minutes/learnspringframework/examples/a1/DepInjectionLauncherAppliaction.java
+++ spring_boot_02/src/main/java/com/in28minutes/learnspringframework/examples/a1/DepInjectionLauncherAppliaction.java
@@ -12,12 +12,33 @@ import java.util.Arrays;
 class YourBusinessClass{
     //생성자나 setter 없이 자동 필드 주입
 
-    @Autowired
     Dependency1 dependency1;
 
-    @Autowired
+
     Dependency2 dependency2;
 
+    public Dependency1 getDependency1() {
+        return dependency1;
+    }
+
+    @Autowired //Setter 에 Autowired 작성해도
+    //@Autowired
+    //Dependency1 dependency1 이랑 같은 효과 ( 의존성 주입 )
+    public void setDependency1(Dependency1 dependency1) {
+        System.out.println("Setter Injection - setDependency1");
+        this.dependency1 = dependency1;
+    }
+
+
+    public Dependency2 getDependency2() {
+        return dependency2;
+    }
+
+    @Autowired
+    public void setDependency2(Dependency2 dependency2) {
+        System.out.println("Setter Injection - setDependency2");
+        this.dependency2 = dependency2;
+    }
 
     public String toString(){
         return "Using "+ dependency1 +" and " + dependency2;
