702286de70f99462f9d953257b33745fc593f3e1
22sonamu <22sonamu@naver.com>
2023-06-01 10:22:27 +0900

    예제 실습 #1

---
+++ spring_boot_02/src/main/java/com/in28minutes/learnspringframework/examples/c1/DataService.java
@@ -0,0 +1,9 @@
+package com.in28minutes.learnspringframework.examples.c1;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface DataService {
+    int[] retrieveData();
+
+}
--- spring_boot_02/src/main/java/com/in28minutes/learnspringframework/examples/c1/RealWorldSpringContextLauncherAppliaction.java
+++ spring_boot_02/src/main/java/com/in28minutes/learnspringframework/examples/c1/RealWorldSpringContextLauncherAppliaction.java
@@ -1,21 +1,22 @@
-package com.in28minutes.learnspringframework.examples.a0;
+package com.in28minutes.learnspringframework.examples.c1;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
 @Configuration
 @ComponentScan
-public class DepInjectionLauncherAppliaction {
+public class RealWorldSpringContextLauncherAppliaction {
 
     public static void main(String[] args) {
 
-        try(var context = new AnnotationConfigApplicationContext(DepInjectionLauncherAppliaction.class)){
+        try(var context = new AnnotationConfigApplicationContext(RealWorldSpringContextLauncherAppliaction.class)){
             Arrays.stream(context.getBeanDefinitionNames())
                     .forEach(System.out::println); //Spring에 필수적인 Bean들을 확인할수있다.
+
+            System.out.println(context.getBean(BusinessCalculationService.class).findMax());
         }
 
     }
