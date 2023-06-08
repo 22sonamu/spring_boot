f035c672b50e2934d45fe33324aaefd23b8912ac
22sonamu <22sonamu@naver.com>
2023-06-08 16:15:41 +0900

    JDBC 활용

--- learn-jpa-and-hibernate/README.md
+++ learn-jpa-and-hibernate/README.md
@@ -12,3 +12,11 @@
 - JDBC 보단 적은 java 코드를 작성한다.
 
 
+~~~h2
+insert into course(id, name, author)
+values (1, 'Learn AWS', 'in28minutes');
+
+select * from course;
+
+delete from course where id=1
+~~~
--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
 
+import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,8 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert();
+        repository.insert(new Course(1, "Lean Java", "in28minutes"));
+        repository.insert(new Course(2, "Lean Python", "in28minutes"));
+        repository.deleteById(1);
     }
 }
