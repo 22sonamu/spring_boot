ba22258628749bbf7c2d999bbaf6ea9c3979239a
22sonamu <22sonamu@naver.com>
2023-06-08 17:00:25 +0900

    JPA #4

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -1,16 +1,19 @@
-package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
+package com.in28minutes.springboot.learnjpaandhibernate.course;
 
 import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
+import com.in28minutes.springboot.learnjpaandhibernate.course.jdbc.CourseJdbcRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 
 @Component
-public class CourseJdbcCommandLineRunner implements CommandLineRunner {
+public class CourseCommandLineRunner implements CommandLineRunner {
 
+//    @Autowired
+//    private CourseJdbcRepository repository;
     @Autowired
-    private  CourseJdbcRepository repository;
+    private CourseJdbcRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
---
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseJpaRepository.java
@@ -0,0 +1,24 @@
+package com.in28minutes.springboot.learnjpaandhibernate.course;
+
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Transactional
+public class CourseJpaRepository {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    public void insert(Course course){
+        entityManager.merge(course);
+    }
+
+    public void deleteById(long id){
+        Course course = entityManager.find(Course.class, id);
+        entityManager.remove(course);
+    }
+
+}
