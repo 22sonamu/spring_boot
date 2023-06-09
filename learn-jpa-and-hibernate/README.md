ec5c4db701c3cf24fb121cc1ceec9cc1d59aaa50
22sonamu <22sonamu@naver.com>
2023-06-09 09:52:06 +0900

    Spring Data JPA #4

--- learn-jpa-and-hibernate/README.md
+++ learn-jpa-and-hibernate/README.md
@@ -11,12 +11,3 @@
 - 많은 SQL 쿼리를 작성해야한다.
 - JDBC 보단 적은 java 코드를 작성한다.
 
-
-~~~h2
-insert into course(id, name, author)
-values (1, 'Learn AWS', 'in28minutes');
-
-select * from course;
-
-delete from course where id=1
-~~~
--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -2,6 +2,7 @@ package com.in28minutes.springboot.learnjpaandhibernate.course;
 
 import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
 import com.in28minutes.springboot.learnjpaandhibernate.course.jdbc.CourseJdbcRepository;
+import com.in28minutes.springboot.learnjpaandhibernate.course.springdatajpa.CoursSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,15 +13,18 @@ public class CourseCommandLineRunner implements CommandLineRunner {
 
 //    @Autowired
 //    private CourseJdbcRepository repository;
+   /* @Autowired
+    private CourseJpaRepository repository;*/
+
     @Autowired
-    private CourseJpaRepository repository;
+    private CoursSpringDataJpaRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert(new Course(1, "Lean Java", "in28minutes"));
-        repository.insert(new Course(2, "Lean Python",  "in28minutes"));
-        repository.deleteById(1);
+        repository.save(new Course(1, "Lean Java", "in28minutes"));
+        repository.save(new Course(2, "Lean Python",  "in28minutes"));
+        repository.deleteById(1l);
 
-        System.out.println(repository.findById(2));
+        System.out.println(repository.findById(2l));
     }
 }
