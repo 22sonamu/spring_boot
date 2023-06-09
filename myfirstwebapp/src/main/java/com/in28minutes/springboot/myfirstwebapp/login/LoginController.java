752535d07d12c71bba9364a1f1700edaef2fb2bc
22sonamu <22sonamu@naver.com>
2023-06-09 15:53:36 +0900

    login 시작 #5

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,23 +1,14 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-    //sout보다 logger가 권장된다.
-    private Logger logger = LoggerFactory.getLogger(getClass());
     //Model
     @RequestMapping("login")
-    public String gotoLoginPage(@RequestParam String name , ModelMap model){
-        //jsp 파일로 전달해주기
-        model.put("name", name);
-        logger.debug("RequestParam is {}" , name);
+    public String gotoLoginPage(){
         return "login";
     }
 }
--- myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -5,6 +5,13 @@
     </title>
 </head>
 <body>
-${name}, login page body
+login page body
+    <!--post로 설정하지 않으면 url에 이름과 password가 노출된다.-->
+    <form method="post">
+        Name : <input type="text" name="name">
+        Password : <input type="password" name = "password">
+        <input type="submit">
+
+    </form>
 </body>
 </html>
