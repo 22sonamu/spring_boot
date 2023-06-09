5729d16cd30471b26f68101e31d4e13d1e11ad84
22sonamu <22sonamu@naver.com>
2023-06-09 10:21:59 +0900

    @Controller, @RequestMapping, @ResponseBody #5

---
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -0,0 +1,15 @@
+package com.in28minutes.springboot.myfirstwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+
+    @RequestMapping("say-hello")
+    @ResponseBody //문자열 View를 Return해준다.
+    public String sayHello(){
+        return "Hello!";
+    }
+}
