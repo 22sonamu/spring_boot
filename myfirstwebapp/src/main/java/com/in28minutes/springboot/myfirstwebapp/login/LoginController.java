20b2debae38040b7a2b80319d3fd8159ee5eeccb
22sonamu <22sonamu@naver.com>
2023-06-09 15:02:52 +0900

    패키지마다 로그 레벨 다르게 설정하기 #4

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +10,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+    //sout보다 logger가 권장된다.
+    private Logger logger = LoggerFactory.getLogger(getClass());
     //Model
     @RequestMapping("login")
     public String gotoLoginPage(@RequestParam String name , ModelMap model){
         //jsp 파일로 전달해주기
         model.put("name", name);
-        System.out.println("RequestParam is " + name);
+        logger.debug("RequestParam is {}" , name);
         return "login";
     }
 }
