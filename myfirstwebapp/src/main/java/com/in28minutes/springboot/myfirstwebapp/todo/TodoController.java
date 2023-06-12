bfe9b727666c8e6bc82352a472a9d4bd83ff82ca
22sonamu <22sonamu@naver.com>
2023-06-12 09:55:21 +0900

    Todo 등록 페이지 컨트롤러, jsp작성 #5

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -3,6 +3,7 @@ package com.in28minutes.springboot.myfirstwebapp.todo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.List;
@@ -25,4 +26,17 @@ public class TodoController {
 
         return "listTodos";
     }
+
+    @RequestMapping(value = "add-todo" , method = RequestMethod.GET)
+    public String showTodoPage(){
+        return "todo";
+    }
+
+    @RequestMapping(value = "add-todo" , method = RequestMethod.POST)
+    public String showNewTodoPage(){
+        // 투두list를 설정해주는 중복 코드를 없애기 위해 list-todos url로 바로 리디렉션한다.
+        return "redirect:list-todos";
+    }
+
+
 }
