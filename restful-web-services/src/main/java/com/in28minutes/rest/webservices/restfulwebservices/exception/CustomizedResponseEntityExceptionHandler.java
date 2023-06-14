caa37dcb72bf4d746250b5a73c8a01a27dd3851b
22sonamu <22sonamu@naver.com>
2023-06-14 11:22:52 +0900

    ResponseEntityExceptionHandler를 상속하여 예외 처리 구현 #6

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.in28minutes.rest.webservices.restfulwebservices.exception;
+
+import com.in28minutes.rest.webservices.restfulwebservices.user.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+//클래스를 모든 컨트롤러에 적용한다.
+@ControllerAdvice
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+    //ResponseEntityExceptionHandler class 의 handleException 함수가 Spring MVC 의 모든 예외 처리를 담당한다.
+    // handleException을 Override하여 excetion을 커스텀 하려 한다.
+
+    @ExceptionHandler(Exception.class) //모든 Exception을 대상으로 한다.
+    public final ResponseEntity<ErrorDetails> handleAllException(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class) //UserNotFoundException을 대상으로 한다.
+    public final ResponseEntity<ErrorDetails> handleUserNotFondException(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity(errorDetails, HttpStatus.NOT_FOUND);
+    }
+}
