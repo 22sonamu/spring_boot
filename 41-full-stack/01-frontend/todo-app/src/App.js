b8b653483f37972200f097d0af04f3be9afe14db
22sonamu <22sonamu@naver.com>
2023-06-16 16:13:33 +0900

    props 사용하기 #7

--- 41-full-stack/01-frontend/todo-app/src/App.js
+++ 41-full-stack/01-frontend/todo-app/src/App.js
@@ -4,12 +4,29 @@ import Counter from './components/counter/Counter';
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <Counter by='1'/>
+      <Counter by='3'/>
+      <Counter by='5'/>
+      
     </div>
   );
 }
 
+// function PlayingWithProps(properties){
+//   console.log(properties.property1)
+//   console.log(properties.property2)
+//   return (
+//     <div>props</div>
+//   )
+// }
 
 
+function PlayingWithProps({property1, property2}){
+  console.log(property1)
+  console.log(property2)
+  return (
+    <div>props</div>
+  )
+}
 
 export default App;
--- 41-full-stack/01-frontend/todo-app/src/components/counter/Counter.jsx
+++ 41-full-stack/01-frontend/todo-app/src/components/counter/Counter.jsx
@@ -1,32 +1,34 @@
 import {useState} from 'react'
 import './Counter.css'
 
-export default function Counter(){
+export default function Counter({by}){
 
     //두개의 요소를 가짐 [0 , f]
     const [count, setCount] = useState(0);
 
+    console.log(by);
+
     function incrementCountFunction(){
-        setCount(count+1);
+        setCount(count+by);
         console.log(count);
     }
 
     function decreaseCountFunction(){
-        setCount(count-1);
+        setCount(count-by);
         console.log(count);
-    }
+    } 
 
-
+    
     return (
         <div className="Counter">
             <span className="count">{count}</span>
             <div></div>
             <button className="counterButton" 
                     onClick={incrementCountFunction}  
-                    >+1</button>
+                    >{by}</button>
             <button className="counterButton" 
                     onClick={decreaseCountFunction}  
-                    >-1</button>
+                    >-{by}</button>
         </div>
     )
 }
