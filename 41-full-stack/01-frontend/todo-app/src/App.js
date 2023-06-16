c74d76a9e1a03fbd6dc0fe0221da11355e4b868c
22sonamu <22sonamu@naver.com>
2023-06-16 17:02:27 +0900

    함수를 하위로 전달하여 상위 state 관리하기 #7

--- 41-full-stack/01-frontend/todo-app/src/App.js
+++ 41-full-stack/01-frontend/todo-app/src/App.js
@@ -1,13 +1,13 @@
 
 import './App.css';
 import Counter from './components/counter/Counter';
+
+
+
 function App() {
   return (
     <div className="App">
-      <Counter by='1'/>
-      <Counter by='3'/>
-      <Counter by='5'/>
-      
+      <Counter></Counter>
     </div>
   );
 }
--- 41-full-stack/01-frontend/todo-app/src/components/counter/Counter.jsx
+++ 41-full-stack/01-frontend/todo-app/src/components/counter/Counter.jsx
@@ -1,7 +1,31 @@
 import {useState} from 'react'
 import './Counter.css'
+import {PropTypes} from 'prop-types'
+export default function Counter(){
 
-export default function Counter({by}){
+    const [count, setCount] = useState(0);
+
+    function increamentCounterParentFunction(by){
+        setCount(count+by);
+    }
+    function decreaseCounterParentFunction(by){
+        setCount(count-by);
+    }
+
+
+    return (
+        <div>
+            <span className="totalCount">{count}</span>
+            <CounterButton by={1} increamentCounterParentFunction={increamentCounterParentFunction} decreaseCounterParentFunction={decreaseCounterParentFunction}/>
+            <CounterButton by={2} increamentCounterParentFunction={increamentCounterParentFunction} decreaseCounterParentFunction={decreaseCounterParentFunction}/>
+            <CounterButton by={5} increamentCounterParentFunction={increamentCounterParentFunction} decreaseCounterParentFunction={decreaseCounterParentFunction}/>
+            <CounterButton></CounterButton>
+        </div> 
+    )
+}
+
+
+export function CounterButton({by, increamentCounterParentFunction, decreaseCounterParentFunction}){ //하위 컴포넌트에서는 상위 컴포넌트의 메서드에 접근할 수없다.
 
     //두개의 요소를 가짐 [0 , f]
     const [count, setCount] = useState(0);
@@ -10,13 +34,13 @@ export default function Counter({by}){
 
     function incrementCountFunction(){
         setCount(count+by);
-        console.log(count);
+        increamentCounterParentFunction(by);
     }
 
     function decreaseCountFunction(){
         setCount(count-by);
-        console.log(count);
-    } 
+        decreaseCounterParentFunction(by);
+    }  
 
     
     return (
@@ -31,4 +55,12 @@ export default function Counter({by}){
                     >-{by}</button>
         </div>
     )
+}
+
+CounterButton.proTypes = {
+    by : PropTypes.number
+}
+
+CounterButton.defaultProps= {
+    by:1
 }
