eade6bcd523dbe4a7612f037b7d48fc6316257f6
22sonamu <22sonamu@naver.com>
2023-06-19 13:52:48 +0900

    Error Component 작성 #7

--- 41-full-stack/01-frontend/todo-app/src/components/Todo/TodoApp.jsx
+++ 41-full-stack/01-frontend/todo-app/src/components/Todo/TodoApp.jsx
@@ -9,6 +9,8 @@ export default function TodoApp(){
                     <Route path='/' element={<LoginComponent/>}></Route>
                     <Route path='/login' element={<LoginComponent/>}></Route>
                     <Route path='/welcome' element={<WelcomeComponent/>}></Route>
+                    {/* 위 루트중 아무곳에도 해당하지않으면  */}
+                    <Route path='*' element={<ErrorComponent/>}></Route>
                 </Routes>
             </BrowserRouter>
         </div>
@@ -49,6 +51,7 @@ function LoginComponent(){
 
     return(
         <div className="Login">
+            <h1>Time to Login</h1>
             {/* showSuccessMessage가 true이면 뒤의 div를 보여주겠다는 뜻 */}
             {showSuccessMessage && <div className='successMessage'>Authenticated Successfully</div>}
             {showErrorMessage && <div className='errorMessage'>Authenticated Failed</div>}
@@ -73,8 +76,20 @@ function LoginComponent(){
 
 function WelcomeComponent(){
     return(
-        <div className="Welcome">
-            Welcome Component
+        <div>
+            <h1>Welcome in28minutes</h1>
+            <div className="Welcome">
+                Welcome Component
+            </div>
+        </div>
+    )
+}
+
+function ErrorComponent(){
+    return (
+        <div className="ErrorComponent">
+            <h1>We are Working really hard!</h1>
+            <div>Sorry</div>
         </div>
     )
 }
