a48f6c8eafd368d7eda6e7760421445670cc1b42
22sonamu <22sonamu@naver.com>
2023-06-19 11:00:43 +0900

    input field의 value handle하기 #7

--- 41-full-stack/01-frontend/todo-app/src/components/Todo/TodoApp.jsx
+++ 41-full-stack/01-frontend/todo-app/src/components/Todo/TodoApp.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import './TodoApp.css'
 export default function TodoApp(){
     return (    
         <div className="TodoApp">
@@ -9,16 +11,29 @@ export default function TodoApp(){
 }
 
 function LoginComponent(){
+
+    const [username, setUsername] = useState("in28minutes")
+
+    const [password, setPassword] = useState("")
+
+    function handleUsernameChange(event){
+        setUsername(event.target.value)
+    }
+
+    function handlePasswordChange(event){
+        setPassword(event.target.value)
+    }
+
     return(
         <div className="Login">
             <div className="LoginForm">
                 <div>
                     <label>User Name</label>
-                    <input type="text" name="username"></input>
+                    <input type="text" name="username" value={username} onChange={handleUsernameChange}></input>
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password" name="password"></input>
+                    <input type="password" name="password" value={password} onChange={handlePasswordChange}></input>
                 </div>
                 <div>
                     <button type="button" name="login"> login </button>
