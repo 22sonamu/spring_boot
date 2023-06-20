f24b3f5e5bc0f113d9650acfa3a868ff48a5c771
22sonamu <22sonamu@naver.com>
2023-06-20 15:16:14 +0900

    주석 추가 #7

--- 41-full-stack/01-frontend/todo-app/src/components/Todo/api/HelloWorldApiService.jsx
+++ 41-full-stack/01-frontend/todo-app/src/components/Todo/api/HelloWorldApiService.jsx
@@ -15,7 +15,7 @@ export const retrieveHelloWorldBean = () => apiClient.get('/hello-world-bean')
 
 export const retrieveHelloWorldPathVariable = (username) => apiClient.get(`/hello-world/path-variable/${username}`, {
     headers: {
-        // Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource. => 인증 header 때문에 생긴 에러
+        // Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource. => 인증 header 때문에 생긴 에러 (프리플라이트 요청이 엑세스 제어 체크를 통과하지 못하는중)
         // 프리플라이트 요청 = option 요청 이 먼저 요청되고 , 그 후에 실제 요청이 전송된다.
         // 1. 모든 사람에게 option 요청에 대한 엑세스를 허용해 줘야 한다.
         // 2. 로그인 시점에 백엔드에 테스트 요청을 보내야한다. (토큰을 생성한다.)
