6f19f6323d5ca4248c95dd37950d46c542181eed
22sonamu <22sonamu@naver.com>
2023-06-20 10:00:09 +0900

    hello world / hello world bean api 요청하기 #7

--- 41-full-stack/01-frontend/todo-app/src/components/Todo/WelcomeComponent.jsx
+++ 41-full-stack/01-frontend/todo-app/src/components/Todo/WelcomeComponent.jsx
@@ -1,29 +1,46 @@
 import {Link, useParams} from 'react-router-dom';
 import axios from 'axios'
+import { useState } from 'react';
 export default function WelcomeComponent(){
 
     const {username} = useParams()
-
+    const [message, setMessage] = useState(null)
     // const params = useParams()
     // console.log(params.username) //위와 같은 뜻임
 
     function callHelloWorldRestApi(){
         //axios
-        axios.get('http://localhost:8080/hello-world')
-            .then(
-                //만약 response가 돌아오면 successfulResponse를 실행한다.
-                (response) => successfulResponse(response)
-            )
-            .catch(
-                (error) => errorResponse(error)
-            ) 
-            .finally( // 요청 성공 여부와 무관
-                () => console.log('cleanup')
-            )
+        //localhost:3000 -> localhost:8080 호출 (Cross Origin Request) -> denied
+        //Allow all requests only from http://localhost:3000 이라고 설정해줘야한다. (rest api project에서)
+        // axios.get('http://localhost:8080/hello-world')
+        //     .then(
+        //         //만약 response가 돌아오면 successfulResponse를 실행한다.
+        //         (response) => successfulResponse(response)
+        //     )
+        //     .catch(
+        //         (error) => errorResponse(error)
+        //     ) 
+        //     .finally( // 요청 성공 여부와 무관
+        //         () => console.log('cleanup')
+        //     )
+
+        axios.get('http://localhost:8080/hello-world-bean')
+        .then(
+            //만약 response가 돌아오면 successfulResponse를 실행한다.
+            (response) => successfulResponse(response)
+        )
+        .catch(
+            (error) => errorResponse(error)
+        ) 
+        .finally( // 요청 성공 여부와 무관
+            () => console.log('cleanup')
+        )
 
     }
     function successfulResponse(response){
         console.log(response)
+        //setMessage(response.data)
+        setMessage(response.data.message)
     }
     function errorResponse(error){
         console.log(error)
@@ -37,6 +54,9 @@ export default function WelcomeComponent(){
             </div>
             <div>
                 <button className='btn btn-success m-S' onClick={callHelloWorldRestApi}>Call Hello World REST API</button>
+            </div>
+            <div className='text-info'>
+                {}
             </div>
         
         </div>
