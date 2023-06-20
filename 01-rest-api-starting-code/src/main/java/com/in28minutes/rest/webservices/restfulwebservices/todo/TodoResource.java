980cd2e2a0f81fc93920da22e108f0fa43462dd6
22sonamu <22sonamu@naver.com>
2023-06-20 10:50:25 +0900

    유저별 todo list 조회 / 유저별 특정 todo 조회 api #7

--- 01-rest-api-starting-code/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ 01-rest-api-starting-code/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -1,2 +1,24 @@
-package com.in28minutes.rest.webservices.restfulwebservices.todo;public class TodoResource {
+package com.in28minutes.rest.webservices.restfulwebservices.todo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TodoResource {
+    private TodoService todoService;
+    public TodoResource(TodoService todoService){
+        this.todoService = todoService;
+    }
+    @GetMapping("/users/{username}/todos")
+    public List<Todo> retrieveTodos(@PathVariable String username){
+        return todoService.findByUsername(username);
+    }
+
+    @GetMapping("/users/{username}/todos/{id}")
+    public Todo retrieveTodo(@PathVariable String username , @PathVariable int id){
+        return todoService.findById(id);
+    }
 }
