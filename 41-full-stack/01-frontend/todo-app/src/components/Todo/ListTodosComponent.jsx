c97d30815b6843b0a658d65297ef1d3739db021e
22sonamu <22sonamu@naver.com>
2023-06-20 10:47:40 +0900

    todos 페이지에 api 연결 , useEffect #7

--- 41-full-stack/01-frontend/todo-app/src/components/Todo/ListTodosComponent.jsx
+++ 41-full-stack/01-frontend/todo-app/src/components/Todo/ListTodosComponent.jsx
@@ -1,14 +1,29 @@
+import { useEffect, useState } from "react"
+import { retrieveAllTodosForUsername } from "./api/TodoApiService"
+
 export default function ListTodosComponent(){
     const today = new Date()
     const targetDate = new Date(today.getFullYear()+12, today.getMonth(), today.getDate())
 
     console.log(today.toDateString)
+    const [todos, setTodos] = useState([])
 
-    const todos = [
-        {id : 1, description :'Learn AWS', done : false, targetDate:targetDate },
-        {id : 2, description :'Learn Full Stack Dev', done : false, targetDate:targetDate},
-        {id : 3, description :'Learn DevOps', done :false, targetDate:targetDate}
-    ]
+    function refreshTodos(){
+        retrieveAllTodosForUsername('in28minutes')
+        .then(
+            (response) => {
+                console.log(response)
+                setTodos(response.data)
+            }
+        ).catch(    
+            error => console.log(error)
+        )
+    }
+
+    useEffect (
+        () => refreshTodos(), [] //첫번째에만 로딩하겠다는 뜻
+    )
+
     return (
         <div className="container">
             <h1>Things You Want to Do!</h1>
@@ -30,7 +45,7 @@ export default function ListTodosComponent(){
                                         <td>{todo.id}</td>
                                         <td>{todo.description}</td>
                                         <td>{todo.done.toString()}</td>
-                                        <td>{todo.targetDate.toDateString()}</td>
+                                        <td>{todo.targetDate}</td>
                                     </tr>
                                 )
                             )
