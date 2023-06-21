7170f6adbfb3286e1a68a01e5abc81cb50c40ea4
22sonamu <22sonamu@naver.com>
2023-06-21 10:32:12 +0900

    h2 연결 #7

---
+++ 01-rest-api-starting-code/target/classes/data.sql
@@ -0,0 +1,9 @@
+insert into todo(id, description, done, target_date, username)
+values (10001, 'lean aws', false, current_date(), 'in28minutes');
+
+
+insert into todo(id, description, done, target_date, username)
+values (10002, 'lean aws1', false, current_date(), 'in28minutes');
+
+insert into todo(id, description, done, target_date, username)
+values (10003, 'lean aws2', false, current_date(), 'in28minutes');
