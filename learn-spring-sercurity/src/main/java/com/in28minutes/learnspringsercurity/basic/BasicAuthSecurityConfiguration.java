b733174ddbc46ac5fc92d126349ea7ee30984cf0
22sonamu <22sonamu@naver.com>
2023-06-22 14:50:35 +0900

    별도의 유저 생성 #9

--- learn-spring-sercurity/src/main/java/com/in28minutes/learnspringsercurity/basic/BasicAuthSecurityConfiguration.java
+++ learn-spring-sercurity/src/main/java/com/in28minutes/learnspringsercurity/basic/BasicAuthSecurityConfiguration.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -24,5 +27,21 @@ public class BasicAuthSecurityConfiguration {
         http.httpBasic(withDefaults());
         http.csrf().disable(); //csrf 설정 disable
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){ //사용자 정보 가져오는 Interface
+        var user = User.withUsername("in28minutes")
+                .password("{noop}dummy")//{noop} -> 인코딩 x
+                .roles("USER")
+                .build();
+
+        var admin = User.withUsername("admin")
+                .password("{noop}dummy")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+
     }
 }
