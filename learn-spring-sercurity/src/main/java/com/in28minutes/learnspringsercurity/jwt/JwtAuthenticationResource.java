16d83a973b678e2dff1ce189b3b91e03ecac3423
22sonamu <22sonamu@naver.com>
2023-06-23 14:35:01 +0900

    jwt 토큰 생성 api #9

--- learn-spring-sercurity/src/main/java/com/in28minutes/learnspringsercurity/jwt/JwtAuthenticationResource.java
+++ learn-spring-sercurity/src/main/java/com/in28minutes/learnspringsercurity/jwt/JwtAuthenticationResource.java
@@ -1,15 +1,28 @@
 package com.in28minutes.learnspringsercurity.jwt;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
+import java.util.stream.Collectors;
+
 @RestController
 public class JwtAuthenticationResource {
+    private JwtEncoder jwtEncoder;
+    private JwtAuthenticationResource(JwtEncoder jwtEncoder){
+        this.jwtEncoder = jwtEncoder;
+    }
+
+
 
     @PostMapping("/authenticate")
-    public Authentication authenticate(Authentication authentication){
-        return authentication;
+    public JwtResponse authenticate(Authentication authentication) {
+        return new JwtResponse(createToken(authentication));
+//        return authentication 이면 :
 //        {
 //            "authorities": [
 //            {
@@ -37,5 +50,26 @@ public class JwtAuthenticationResource {
 //            "credentials": null,
 //                "name": "in28minutes"
 //        }
-//    }
+    }
+
+    private String createToken(Authentication authentication) {
+        var claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(Instant.now())
+                .expiresAt(Instant.now().plusSeconds(60 * 15)) //jwt 만료 시간
+                .subject(authentication.getName())
+                .claim("scope", createScope(authentication))
+                .build();
+
+        JwtEncoderParameters parameters = JwtEncoderParameters.from(claims); //claims로부터 파라미터를 만들고, 인코딩한후 getToken해서 Return 한다.
+        return jwtEncoder.encode(parameters).getTokenValue();
+    }
+
+    private Object createScope(Authentication authentication) {
+        return authentication.getAuthorities().stream()
+                .map(a -> a.getAuthority())
+                .collect(Collectors.joining(" ")); //모든 권한을 공백으로 구분해서 취합한다.
+    }
 }
+
+record JwtResponse(String token){}
