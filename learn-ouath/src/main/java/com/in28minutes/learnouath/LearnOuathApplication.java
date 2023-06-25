e931b0c434358e52dda7fab950f68f913e07ff28
22sonamu <22sonamu@naver.com>
2023-06-25 14:20:24 +0900

    Oauth 프로젝트 생성 #9

---
+++ learn-ouath/src/main/java/com/in28minutes/learnouath/LearnOuathApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.learnouath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnOuathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnOuathApplication.class, args);
+	}
+
+}
---
+++ learn-ouath/src/test/java/com/in28minutes/learnouath/LearnOuathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.learnouath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnOuathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
